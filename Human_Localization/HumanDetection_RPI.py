781528ef51f3e32ef8adadafbb3e1c5f5f75921e
CuriousCoder <79039613+Bhushan-Bhad@users.noreply.github.com>
2024-07-17 15:17:36 +0200

    chnages for YOLO

--- Human_Localization/HumanDetection_RPI.py
+++ Human_Localization/HumanDetection_RPI.py
@@ -1,4 +1,5 @@
 import cv2
+from ultralytics import YOLO
 
 # List available video devices
 def list_video_devices():
@@ -16,7 +17,7 @@ def list_video_devices():
 
 # Print available video devices
 devices = list_video_devices()
-print("Available video devices:", devices)
+# print("Available video devices:", devices)
 
 # Use the correct index for your external webcam
 external_webcam_index = 0  # Replace with the correct index if needed
@@ -33,7 +34,18 @@ if not cap.isOpened():
 
 try:
     # Your remaining code here
-
+    model = YOLO("yolo-Weights/yolov8n.pt")
+    classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
     while True:
         # Capture frame-by-frame
         ret, frame = cap.read()
@@ -42,7 +54,34 @@ try:
         if not ret:
             print("Failed to capture frame.")
             break
+        results = model(frame, stream = True)
+        for r in results:
+            boxes = r.boxes
 
+            for box in boxes:
+                # bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+                # put box in cam
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+                # confidence
+                confidence = math.ceil((box.conf[0]*100))/100
+                print("Confidence --->", confidence)
+
+                # class name
+                cls = int(box.cls[0])
+                print("Class name -->", classNames[cls])
+
+                # object details
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+
+                cv2.putText
         # Display the frame
         cv2.imshow('Webcam', frame)
 
